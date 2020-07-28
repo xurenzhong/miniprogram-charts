2319cb45df0f91a617af63d8b48978131f01357e
许任重 <xurzh@corp.21cn.com>
2020-07-28 17:56:34 +0800

    mod: 增加设备详情的获取

--- pages/deviceState/deviceState.js
+++ pages/deviceState/deviceState.js
@@ -1,17 +1,27 @@
-Page({
+var http = require('../../libs/httputils.js')
 
+const app = getApp()
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    
+    username: wx.getStorageSync('username'),
+    chip_id: '',
+    fault:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 读取上级传递信息
+    var chip_id = options.chip_id
+    var fault = options.fault
+    this.setData({chip_id: chip_id, fault: fault})
+    // 拿取设备数据的详情
+    this.initData(chip_id)
   },
 
   /**
@@ -62,6 +72,24 @@ Page({
   onShareAppMessage: function () {
     
   },
+
+  /**
+   * 加载设备详情
+   */
+  initData: function (chip_id) {
+    let that = this
+    var params = {
+      chip_id: chip_id,
+      username: that.data.username
+    }
+
+    http.getRequest(app.globalData.root + "/programs/deviceInfoServlet", params, function (res) {
+      console.log(res)
+    }, function (err) {
+      console.log(err)
+    })
+  },
+
   // 保养提醒
   waterWarning: function () {
     wx.navigateTo({
